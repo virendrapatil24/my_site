52b578f2d299e97a417a2e13ed89dfb85ebee383
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-05-16 00:53:38 +0530

    added admin management for comments section

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -9,6 +10,5 @@ class CommentForm(forms.ModelForm):
         labels = {
             "user_name": "Your Name",
             "user_email": "Your Email",
-            "text": "Your Comment"
+            "text": "Your Comment",
         }
-
